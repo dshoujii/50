620a7c5c68ffc95ef5c0bd08d0ecd198dcce507e
duet <1335226237@qq.com>
2022-09-05 11:19:43 +0800

    初始化

---
+++ expending cards/index.js
@@ -0,0 +1,16 @@
+const imgs = document.querySelectorAll('.img');
+imgs.forEach(img => {
+    img.addEventListener('click', () => {
+        removeActiveClasses();
+        img.classList.add('active');
+    })
+});
+
+function removeActiveClasses() {
+    imgs.forEach(img => {
+        img.classList.remove('active');
+    })
+}
+function abc() {
+    console.log(abc);
+}
